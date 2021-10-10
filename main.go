c6e82ce063073245229726688b3de1268f5406b6
Vachira J <cyberwizard.dev@gmail.com>
2021-10-10 16:11:32 +0700

    sets up server

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+	"net/http"
 )
 
 type Person struct {
@@ -11,30 +9,42 @@ type Person struct {
 }
 
 func main() {
-	p1 := Person{
-		First: "John",
-	}
+	// p1 := Person{
+	// 	First: "John",
+	// }
 
-	p2 := Person{
-		First: "Jan",
-	}
+	// p2 := Person{
+	// 	First: "Jan",
+	// }
 
-	xp := []Person{p1, p2}
+	// xp := []Person{p1, p2}
 
-	bs, err := json.Marshal(xp)
-	if err != nil {
-		log.Panic(err)
-	}
+	// bs, err := json.Marshal(xp)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
 
-	fmt.Println(string(bs))
+	// fmt.Println(string(bs))
 
-	xp2 := []Person{}
+	// xp2 := []Person{}
 
-	err = json.Unmarshal(bs, &xp2)
-	if err != nil {
-		log.Panic(err)
-	}
+	// err = json.Unmarshal(bs, &xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
 
-	fmt.Println("back into a Go data structure", xp2)
+	// fmt.Println("back into a Go data structure", xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
 
 }
