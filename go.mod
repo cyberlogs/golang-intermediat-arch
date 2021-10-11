261d8797e912c08e9c03b369220c384caf260b88
Vachira J <cyberwizard.dev@gmail.com>
2021-10-12 00:59:24 +0700

    Hashing example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cyberlogs/golang-intermediat-arch
 
 go 1.17
+
+require golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-type Person struct {
-	First string `json:"first"`
+func main() {
+	pass := "123456789"
+	hashPass, err := hashPassword(pass)
+	if err != nil {
+		panic(err)
+	}
+
+	err = comparePassword(pass, hashPass)
+	if err != nil {
+		log.Fatalln("Not logged in")
+	}
+
+	log.Println("Logged in!")
+
 }
 
-func main() {
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("error while generating bcrypt hash from password: %w", err)
+	}
+	return bs, nil
+}
+
+func comparePassword(password string, hashPassword []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashPassword, []byte(password))
+	if err != nil {
+		return fmt.Errorf("invalid password: %w", err)
+	}
+	return nil
 }
